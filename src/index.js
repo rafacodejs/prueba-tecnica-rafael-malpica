892038fc2575e87bb50bea247a511ce1b769d749
Rafael Malpica <rafacodejs@gmail.com>
2023-01-19 19:51:38 -0400

    Feat: Implementación de Header y Navbar para Desktop y Mobile

---
+++ src/index.js
@@ -0,0 +1,31 @@
+let position = 1;
+
+function slidePosition(n) {
+  showSlides((position = n));
+}
+
+function nextSlide(n) {
+  showSlides((position += n));
+}
+
+setInterval(function time() {
+  showSlides((position += 1));
+}, 4000);
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('slide');
+
+  if (n > slides.length) {
+    position = 1;
+  }
+  if (n < 1) {
+    position = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[position - 1].style.display = 'block';
+}
+
+showSlides(position);
