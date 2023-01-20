89b585d3c2171a6b8afce7c2588a3dc4e2a98295
Rafael Malpica <rafacodejs@gmail.com>
2023-01-20 15:43:27 -0400

    Docs: cambio de nombre en images

--- src/index.js
+++ src/index.js
@@ -28,3 +28,23 @@ function showSlides(n) {
   slides[position - 1].style.display = 'block';
 }
 showSlides(position);
+
+$('.owl-carousel').owlCarousel({
+  margin: 15,
+  nav: true,
+  navText: [
+    "<div class='nav-button owl-prev'>‹</div>",
+    "<div class='nav-button owl-next'>›</div>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
